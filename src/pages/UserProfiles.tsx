667103dc1a78dda0a488b49463113e7c2ea6c720
alexanderkolosov <avk7961555@gmail.com>
2025-02-19 19:25:55 +0100

    trying to fetch users data

--- src/pages/UserProfiles.tsx
+++ src/pages/UserProfiles.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import PageBreadcrumb from "../components/common/PageBreadCrumb";
-import UserMetaCard from "../components/UserProfile/UserMetaCard";
+
 import UserInfoCard from "../components/UserProfile/UserInfoCard";
-import UserAddressCard from "../components/UserProfile/UserAddressCard";
+
 import PageMeta from "../components/common/PageMeta";
 
 export default function UserProfiles() {
@@ -18,9 +18,9 @@ export default function UserProfiles() {
           Profile
         </h3>
         <div className="space-y-6">
-          <UserMetaCard />
+
           <UserInfoCard />
-          <UserAddressCard />
+
         </div>
       </div>
     </>
